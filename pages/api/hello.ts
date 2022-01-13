ec4eca0d71716c2e6183ee714aa21fa6cdc90740
Mateo Álvarez <alvarez.fing@gmail.com>
2022-01-13 13:52:37 -0300

    feat: return users information as a example

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from 'prisma/index'
+import { User } from '@prisma/client'
 
-type Data = {
-  name: string
-}
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<User[]>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const users = await prisma.user.findMany({})
+  return res.status(200).json(users)
 }
