41f2d2e55cf985c178f69eb54b4fb4067059687c
Mateo Álvarez <alvarez.fing@gmail.com>
2022-01-14 16:00:36 -0300

    feat: add static target data

---
+++ data/initTargets.ts
@@ -0,0 +1,101 @@
+import { TargetProp } from 'types/target'
+
+export const homeTargets: TargetProp[] = [
+  {
+    content: {
+      id: '1234',
+      owner: 'Mateo Alvarez',
+      title: 'Ir a un restaurant',
+      date: 'Mar. 18:00hs',
+      description:
+        'Unas pizzas en Panda bar y despues nos vamos para la casa de Pedro!',
+      members: [
+        {
+          user: {
+            name: 'Mateo Alvarez',
+            email: '',
+            image: 'http://placehold.it/40',
+          },
+        },
+        {
+          user: {
+            name: 'Fernando Gonzalez',
+            email: '',
+            image: 'http://placehold.it/40',
+          },
+        },
+        {
+          user: {
+            name: 'Matthew D. Brown',
+            email: '',
+            image: 'http://placehold.it/40',
+          },
+        },
+      ],
+    },
+  },
+  {
+    content: {
+      id: 'ffdsfa',
+      owner: 'Mateo Alvarez',
+      title: 'Noche de Overwatch',
+      date: 'Mar. 22:00hs',
+      description: 'Unas partidas cortas',
+      members: [
+        {
+          user: {
+            name: 'Simon',
+            email: '',
+            image: 'http://placehold.it/40',
+          },
+        },
+        {
+          user: {
+            name: 'Andrea Gonzalez',
+            email: '',
+            image: 'http://placehold.it/40',
+          },
+        },
+        {
+          user: {
+            name: 'Silvia',
+            email: '',
+            image: 'http://placehold.it/40',
+          },
+        },
+      ],
+    },
+  },
+  {
+    content: {
+      id: 'sadfasdfdsaf',
+      owner: 'Mateo Alvarez',
+      title: 'Futbol 5 en el Galpon',
+      date: 'Mar. 12:00hs',
+      description: null,
+      members: [
+        {
+          user: {
+            name: 'Raul',
+            email: '',
+            image: 'http://placehold.it/40',
+          },
+        },
+        {
+          user: {
+            name: 'Maria',
+            email: '',
+            image: 'http://placehold.it/40',
+          },
+        },
+        {
+          user: {
+            name: 'Sofia',
+            email: '',
+            image: 'http://placehold.it/40',
+          },
+        },
+      ],
+    },
+  },
+]
