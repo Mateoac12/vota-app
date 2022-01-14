4b38f42b699cae178136761207f48f30eaea983f
Mateo Álvarez <alvarez.fing@gmail.com>
2022-01-13 21:33:56 -0300

    feat: add login button

--- components/LoginButton/index.tsx
+++ components/LoginButton/index.tsx
@@ -1,11 +1,26 @@
 import { signIn } from 'next-auth/react'
+import Image from 'next/image'
+import { useState } from 'react'
+
+import googleSvg from './google-icon.svg'
 
 const LoginButton: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const handleLogin = () => {
+    setIsLoading(true)
+    signIn('google')
+  }
+
   return (
     <button
-      className='bg-violet-600 text-white py-2 px-6 rounded-full shadow-xl shadow-violet-200'
-      onClick={() => signIn('google')}
+      className={`border font-semibold py-1 px-4 rounded-md hover:opacity-95 flex items-center gap-2 hover:shadow-sm ${
+        isLoading && 'opacity-60 hover:cursor-not-allowed hover:opacity-60'
+      }`}
+      disabled={isLoading}
+      onClick={handleLogin}
     >
+      <Image src={googleSvg} alt='google icon' className='w-2' />
       Iniciar sesión
     </button>
   )
