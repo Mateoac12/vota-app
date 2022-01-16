efdf4271334dfee34b0440a9266c78863283f3e9
Mateo Álvarez <alvarez.fing@gmail.com>
2022-01-16 17:03:37 -0300

    ref: delete console.log

--- components/HandleLoginButton/index.tsx
+++ components/HandleLoginButton/index.tsx
@@ -6,7 +6,6 @@ export const HandleLoginButton: React.FC = () => {
   const { data, status } = useSession()
   const user: any = data?.user
 
-  console.log({ status })
   if (!status || status === 'loading') return <p>Cargando...</p>
   if (status === 'authenticated') return <UserMenu user={user} />
   if (status === 'unauthenticated') return <LoginButton />
