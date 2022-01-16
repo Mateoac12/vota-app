9ed4e939a58a496a2534ed4e00e361349268bdb7
Mateo Álvarez <alvarez.fing@gmail.com>
2022-01-16 16:55:01 -0300

    feat: add service to post ideas

---
+++ services/postIdea.ts
@@ -0,0 +1,44 @@
+interface Props {
+  paramsOfPost: {
+    title: string
+    description: string
+    date: string
+    id: string
+  }
+}
+
+export const postIdea = async ({ paramsOfPost }: Props) => {
+  const { title, description, date, id } = paramsOfPost
+
+  console.log(id)
+
+  const newPost = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/post/create`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        data: {
+          title: title,
+          date: new Date(date),
+          description: description,
+          members: {
+            create: {
+              usersId: [id],
+              users: {
+                connect: [{ id: id }],
+              },
+            },
+          },
+          ownerId: id,
+        },
+      }),
+    }
+  )
+
+  return {
+    newPost,
+  }
+}
