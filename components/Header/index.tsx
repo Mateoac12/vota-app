e5939e443409b1b620bf731ee2b94c43c178a868
Mateo Álvarez <alvarez.fing@gmail.com>
2022-01-16 17:03:21 -0300

    feat: new logo with alfa text

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,13 +1,19 @@
 import Link from 'next/link'
 import { HandleLoginButton } from 'components/HandleLoginButton'
+import { TopHeader } from 'components/TopHeader'
 
 export const Header: React.FC = () => {
   return (
-    <header className='flex justify-between w-full max-w-screen-lg mx-auto py-2'>
-      <Link href='/'>
-        <a className='font-semibold text-xl'>Vota.app</a>
-      </Link>
-      <HandleLoginButton />
-    </header>
+    <>
+      <TopHeader />
+      <header className='flex justify-between w-full max-w-screen-lg mx-auto py-2 px-4'>
+        <Link href='/'>
+          <a className='font-semibold relative text-xl before:content-["Alfa"] before:px-2 before:py-1 before:text-xs before:absolute before:top-0 before:-right-10 before:bg-green-200 before:rounded-full'>
+            Vota.app
+          </a>
+        </Link>
+        <HandleLoginButton />
+      </header>
+    </>
   )
 }
