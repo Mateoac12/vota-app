dd5cef4ea812e6fbf4a35e68216e9fc98cba878f
Mateo Álvarez <alvarez.fing@gmail.com>
2022-01-16 19:07:27 -0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:option/:path',
-        destination: process.env.CORS_URL,
-      },
-    ]
-  },
 }
