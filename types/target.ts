1a1020e6715be1da4c104ff7c958f14d88370f3c
Mateo Álvarez <alvarez.fing@gmail.com>
2022-01-16 16:54:47 -0300

    feat: change types

--- types/target.ts
+++ types/target.ts
@@ -1,12 +1,13 @@
 import { User } from './user'
 
 export interface TargetProp {
-  content: {
-    id: string
-    owner: string
-    title: string
-    date: string
-    description?: string | null
-    members: User[]
+  id: string
+  owner: User
+  ownerId: string
+  title: string
+  date: string
+  description?: string | null
+  members: {
+    users: User[]
   }
 }
