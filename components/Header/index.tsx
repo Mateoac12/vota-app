02d4f59e903058447760fd9a8b32f2d957c2a793
Mateo Álvarez <alvarez.fing@gmail.com>
2022-01-18 15:30:35 -0300

    feat: align header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -6,7 +6,7 @@ export const Header: React.FC = () => {
   return (
     <>
       <TopHeader />
-      <header className='flex justify-between w-full max-w-screen-lg mx-auto py-2 px-4'>
+      <header className='flex justify-between w-full max-w-screen-lg mx-auto py-2 px-4 items-center'>
         <Link href='/'>
           <a className='font-semibold relative text-xl before:content-["Alfa"] before:px-2 before:py-1 before:text-xs before:absolute before:top-0 before:-right-10 before:bg-green-200 before:rounded-full'>
             Vota.app
