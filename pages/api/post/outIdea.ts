d77c57787d7fa26ee6241b1cbdab2765b2ec77c4
Mateo Álvarez <alvarez.fing@gmail.com>
2022-01-19 18:22:35 -0300

    feat: add route to disconnet user in idea

---
+++ pages/api/post/outIdea.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from 'prisma'
+
+export default async function outIdea(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { userId, postId } = req.body.data
+
+  const post = await prisma.memberOfPost.update({
+    where: {
+      postId: postId,
+    },
+    data: {
+      users: {
+        disconnect: { id: userId },
+      },
+    },
+  })
+
+  return res.status(200).json(post)
+}
